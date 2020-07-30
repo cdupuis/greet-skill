18633b216b22ef83b28cacdb5a571a1a95a52909
Christian Dupuis <cd@atomist.com>
2020-07-30 13:28:12 +0200

    Better message

--- events/onNewChatUser.js
+++ events/onNewChatUser.js
@@ -1,6 +1,6 @@
 exports.handler = async ctx => {
     const screenName = ctx.data.ChatId[0].screenName;
     await ctx.message.send({
-        text: `Welcome to atomist-community`,
+        text: `Welcome to the atomist-community Slack workspace`,
     }, {users: [screenName]});
 };
