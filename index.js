2dc973cd094e509079eb9bc97651a678c8b47045
Christian Dupuis <cd@atomist.com>
2020-08-04 12:14:16 +0200

    Polish

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-exports.handler = async ctx => {
+const skill = require("@atomist/skill");
+
+const onNewChatUser = async ctx => {
     const screenName = ctx.data.ChatId[0].screenName;
     await ctx.message.send({
         text: `Welcome to the atomist-community Slack workspace`,
     }, {users: [screenName]});
 };
+
+exports.handler = skill.entryPoint({
+    onNewChatUser,
+});
